9c7e1b7d0f301ce12505d2d54e59128f6539a047
Romeran Rodriguez <romeran14@gmail.com>
2022-11-13 21:41:11 -0400

    Comentando error en inputtweets

--- src/components/Tendencies.jsx
+++ src/components/Tendencies.jsx
@@ -1,42 +1,49 @@
-import React from "react";
-import { BsThreeDots } from "react-icons/bs";
+import React, {useEffect} from 'react'
+import { uniqueId } from 'lodash'
+import {BsThreeDots} from "react-icons/bs"
 
 const Tendencies = () => {
-  let alltrends = [];
 
-  const tendencies = {
-    cantidadTw: 1000,
-    toptext: " Lorem ipsum dolor sit",
-    text: "trends in your country",
-  };
+ 
+      let alltrends =  []
 
-  let contador = 0;
+   const tendencies = {
+     cantidadTw: 1000,
+     toptext: ' Lorem ipsum dolor sit',
+     text:'trends in your country'
+   }
+ 
+  let contador =0
   do {
-    contador++;
+    contador ++;
 
-    alltrends.push(tendencies);
+    alltrends.push(tendencies)
+    
   } while (contador < 10);
-
+  
+  useEffect(() => {
+  console.log('alltrends',alltrends)
+  }, [])
+  
   return (
-    <div className="Tendencias">
-      <div className="graybg">
+    <div  className='Tendencias'>
+        <div className="graybg">
         <h3>trends for you</h3>
-        {alltrends.map((item) => (
-          <div key={Math.random()} className="tendencies-item">
-            <div className="left">
-              <p className="toptext">{item.text}</p>
-              <p className="text">{item.toptext}</p>
-              <p className="tweetqty">
-                {Math.round(Math.random() * 1000)} Tweets
-              </p>
+        {alltrends.map(  item => (
+            <div key={Math.random()} className='tendencies-item' >
+                <div className="left">
+                    <p className="toptext">{item.text}</p>
+                    <p className="text">{item.toptext}</p>
+                    <p className="tweetqty">{Math.round(Math.random()*1000)} Tweets</p>
+                </div>
+                <BsThreeDots/>
             </div>
-            <BsThreeDots />
-          </div>
         ))}
-        <a className="show-more">Show More</a>
-      </div>
+        <a className='show-more'>Show More</a>
+</div>
     </div>
-  );
-};
+    
+  )
+}
 
-export default Tendencies;
+export default Tendencies
